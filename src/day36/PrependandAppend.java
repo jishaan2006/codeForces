be45473df4cbdbf30774b55c473053557437109c
Jishaan Khatri <jishaan123465@gmail.com>
2026-07-17 23:05:01 +0530

    Add day36 solutions

---
+++ src/day36/PrependandAppend.java
@@ -0,0 +1,26 @@
+package day36;
+
+import java.util.Scanner;
+
+public class PrependandAppend {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            String s = sc.next();
+
+            int l = 0;
+            int r = n - 1;
+
+            while (l < r && s.charAt(l) != s.charAt(r)) {
+                l++;
+                r--;
+            }
+
+            System.out.println(r - l + 1);
+        }
+    }
+}
