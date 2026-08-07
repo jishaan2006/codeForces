1566f4c490b6b6910038e9474ea13120b108aac0
Jishaan Khatri <jishaan123465@gmail.com>
2026-08-07 22:54:14 +0530

    Add day43 solutions

---
+++ src/day43/Cover_in_water.java
@@ -0,0 +1,32 @@
+package day43;
+
+import java.util.*;
+
+public class Cover_in_water{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            String s = sc.next();
+
+            int dots = 0;
+
+            for (int i = 0; i < n; i++) {
+                if (s.charAt(i) == '.') {
+                    dots++;
+                }
+            }
+
+            if (s.contains("...")) {
+                System.out.println(2);
+            } else {
+                System.out.println(dots);
+            }
+        }
+
+        sc.close();
+    }
+}
